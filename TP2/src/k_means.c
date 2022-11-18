86ed5b57583db4259c789bafeddecc655a4923ba
RayMightBeWrong <ruizinho7braga@gmail.com>
2022-11-18 15:42:20 +0000

    Comentários

--- TP2/src/k_means.c
+++ TP2/src/k_means.c
@@ -16,19 +16,20 @@ int main(int argc, char **argv){
 	float *cx = malloc(sizeof(float) * K);
 	float *cy = malloc(sizeof(float) * K);
 
-	// keeps track of which cluster a point belongs yo
+	// keeps track of which cluster a point belongs to
 	int *point_cluster = malloc(sizeof(int) * N);
 	
-	// keeps track of which cluster a point belongs yo
+	// keeps track of how many points are in a cluster 
 	int *cluster_size = malloc(sizeof(int) * K);
 
 	// init gives random values to points and clusters
-	// algorithm executes the k-means algorithm and prints the information out
 	init(N, K, px, py, cx, cy);
 
-	//
+	// algorithm executes the k-means algorithm and prints the information out
+	// if there are 4 given arguments when executing,
+	// the parallel version of the code is executed
+	// otherwise, the sequential version is executed
 	if (argc == 4){
-		// 
 		int THREADS = atoi(argv[3]);
 		algorithm(N, K, THREADS, px, py, cx, cy, point_cluster, cluster_size);
 	}
